951d984dae6c7475e91c7d282e9cd62589422639
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-10-23 13:43:53 +0000

    fbazaar driver flutter app,initial commit

---
+++ lib/screens/pages/mainhome.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../controllers/localnotification_controller.dart';
+import '../../controllers/logincontroller.dart';
+import '../../controllers/notificationcontroller.dart';
+import '../../controllers/ordercontroller.dart';
+import '../../controllers/profilecontroller.dart';
+
+import '../../statics/appcolors.dart';
+import '../../widgets/components/ordercomponent.dart';
+import '../../widgets/components/storecomponent.dart';
+
+class MainHome extends StatefulWidget {
+  const MainHome({super.key});
+
+  @override
+  State<MainHome> createState() => _MainHomeState();
+}
+
+class _MainHomeState extends State<MainHome> {
+  var storeItems;
+  final ProfileController profileController = Get.find();
+  final NotificationController notificationController = Get.find();
+  final OrderController orderController = Get.find();
+  final LoginController loginController = Get.find();
+  final storage = GetStorage();
+  late String uToken = "";
+
+  late Timer _timer;
+
+  @override
+  void initState() {
+    if (storage.read("token") != null) {
+      uToken = storage.read("token");
+    }
+    scheduleTimers();
+    super.initState();
+  }
+
+  void scheduleTimers() {
+    profileController.getMyProfile(uToken);
+    orderController.getAllMyDeliveredOrders(uToken);
+    orderController.getAllPendingOrders(uToken);
+    orderController.getAllProcessingOrders(uToken);
+    orderController.getAllPickedUpOrders(uToken);
+    orderController.getAllDeliveredOrders(uToken);
+    notificationController.getAllTriggeredNotifications(uToken);
+    notificationController.getAllUnReadNotifications(uToken);
+    notificationController.getAllNotifications(uToken);
+    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      profileController.getMyProfile(uToken);
+      orderController.getAllMyDeliveredOrders(uToken);
+      orderController.getAllPendingOrders(uToken);
+      orderController.getAllProcessingOrders(uToken);
+      orderController.getAllPickedUpOrders(uToken);
+      orderController.getAllDeliveredOrders(uToken);
+      notificationController.getAllTriggeredNotifications(uToken);
+      notificationController.getAllUnReadNotifications(uToken);
+      notificationController.getAllNotifications(uToken);
+      for (var i in notificationController.triggered) {
+        LocalNotificationController().showNotifications(
+          title: i['notification_title'],
+          body: i['notification_message'],
+        );
+      }
+    });
+
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      for (var e in notificationController.triggered) {
+        notificationController.unTriggerNotifications(e["id"], uToken);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(profileController.username,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: defaultTextColor2)),
+        actions: [
+          IconButton(
+            onPressed: () {
+              loginController.logoutUser(uToken);
+            },
+            icon:
+                const Icon(FontAwesomeIcons.signOut, color: defaultTextColor2),
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Get.to(() => const OrderComponent());
+                  },
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.shopping_bag, size: 50),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text("Orders",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      )),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Get.to(() => const StoreComponent());
+                  },
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.storefront, size: 50),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text("Store",
+                                style: TextStyle(fontWeight: FontWeight.bold))
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
